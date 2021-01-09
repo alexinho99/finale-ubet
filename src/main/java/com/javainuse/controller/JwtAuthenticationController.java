b3693e91291316fba81e66e4685ffee02e6c7483
Vladimir Tsvetanov <vladimir.tsvetanov55@gmail.com>
2021-01-09 21:50:07 +0200

    login wrong credentials response fix

--- src/main/java/com/javainuse/controller/JwtAuthenticationController.java
+++ src/main/java/com/javainuse/controller/JwtAuthenticationController.java
@@ -6,13 +6,11 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.javainuse.dao.UserDao;
 import com.javainuse.model.*;
-import io.jsonwebtoken.JwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +18,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -56,7 +55,16 @@ public class JwtAuthenticationController {
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
 
-		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (Exception e) {
+			String result = "";
+			ResponseMessage responseMessage = new ResponseMessage();
+			responseMessage.setToken("null");
+			responseMessage.setMessage("Wrong credentials!");
+			result = printObjecto(responseMessage);
+			return ResponseEntity.ok(result);
+		}
 
 		final UserDetails userDetails = userDetailsService
 				.loadUserByUsername(authenticationRequest.getUsername());
@@ -135,22 +143,6 @@ public class JwtAuthenticationController {
 		}
 
 		return ResponseEntity.ok(result);
-	}
-
-	private void authenticate(String username, String password) throws Exception {
-		try {
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
-		} catch (BadCredentialsException e) {
-			String result = "";
-			ResponseMessage responseMessage = new ResponseMessage();
-			responseMessage.setToken("null");
-			responseMessage.setMessage("Wrong credentials!");
-			result = printObjecto(responseMessage);
-			ResponseEntity.ok(result);
-			e.getMessage();
-		}
 	}
 
 	public String printObjecto(Object object) {
